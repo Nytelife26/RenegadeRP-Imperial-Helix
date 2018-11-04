6d2ec8c45dbd811af04aa4031a74ca88ec10a3b7
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-04 15:46:40 +0000

    Create sh_navy.lua

---
+++ schema/factions/sh_navy.lua
@@ -0,0 +1,38 @@
+FACTION.name = "Imperial Navy"
+FACTION.description = "A Commander of the Imperial Navy"
+FACTION.color = Color(0, 0, 40)
+FACTION.pay = 150
+FACTION.models = {
+    "models/player/scifi_bill.mdl",
+    "models/player/scifi_fang.mdl",
+    "models/player/scifi_female_01.mdl",
+    "models/player/scifi_female_03.mdl",
+    "models/player/scifi_female_02.mdl",
+    "models/player/scifi_female_04.mdl",
+    "models/player/scifi_female_05.mdl",
+    "models/player/scifi_female_06.mdl",
+    "models/player/scifi_female_07.mdl",
+    "models/player/scifi_hawke.mdl",
+    "models/player/scifi_louis.mdl",
+    "models/player/scifi_male_01.mdl",
+    "models/player/scifi_male_02.mdl",
+    "models/player/scifi_male_03mdl",
+    "models/player/scifi_male_04.mdl",
+    "models/player/scifi_male_05.mdl",
+    "models/player/scifi_male_06.mdl",
+    "models/player/scifi_male_07.mdl",
+    "models/player/scifi_male_08.mdl",
+    "models/player/scifi_male_09.mdl",
+    "models/player/scifi_mp1.mdl",
+    "models/player/scifi_mp2.mdl",
+    "models/player/scifi_mp3.mdl",
+    "models/player/scifi_plr.mdl",
+    "models/player/scifi_plr2.mdl",
+    "models/player/scifi_rochelle.mdl",
+    "models/player/scifi_wraith.mdl",
+    "models/player/scifi_zoey.mdl"
+}
+FACTION.isDefault = false
+FACTION.isGloballyRecognized = true
+
+FACTION_NAVY = FACTION.index
