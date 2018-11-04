e45057d4c23696cf9b680c01847b8b508215497e
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-04 17:52:06 +0000

    Create sh_e11dammo.lua

---
+++ schema/items/ammo/sh_e11dammo.lua
@@ -0,0 +1,5 @@
+ITEM.name = "E-11D Pulses"
+ITEM.model = "models/items/combine_rifle_cartridge01.mdl"
+ITEM.ammo = "elld" 
+ITEM.ammoAmount = 65 
+ITEM.ammoDesc = "A Cartridge that contains %s of E-11D Pulses"
