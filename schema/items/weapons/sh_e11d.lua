4a18aa8675f4aafd185183731bf3f9f5c549b7ff
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-04 17:21:54 +0000

    Create sh_e11d.lua

---
+++ schema/items/weapons/sh_e11d.lua
@@ -0,0 +1,12 @@
+ITEM.name = "E-11D Blaster Rifle"
+ITEM.description = "A matte black powerful blaster rifle."
+ITEM.model = "models/farena/weapons/w_e-11d.mdl"
+ITEM.class = "weapon_e11d"
+ITEM.weaponCategory = "primary"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}"
