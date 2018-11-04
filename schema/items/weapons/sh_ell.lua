e26dc0fd5388b662bb8f809762f2ede8420525fc
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-04 19:12:50 +0000

    Create sh_ell.lua

---
+++ schema/items/weapons/sh_ell.lua
@@ -0,0 +1,12 @@
+ITEM.name = "E-11 Blaster Rifle"
+ITEM.description = "A compact, fully automatic Imperial blaster rifle."
+ITEM.model = "models/weapons/w_e11.mdl"
+ITEM.class = "weapon_e11"
+ITEM.weaponCategory = "primary"
+ITEM.width = 3
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.020070368424058, 270.40155029297, 0),
+	fov	= 7.2253324508038,
+	pos	= Vector(0, 200, -1)
+}
