aafed0beea7891c3aaf142b69997e615a06a8621
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-05 22:00:23 +0000

    Create sh_ee3ammo.lua

---
+++ schema/items/ammo/sh_ee3ammo.lua
@@ -0,0 +1,9 @@
+
+ITEM.name = "EE-3 Cartridges"
+ITEM.model = "models/Items/combine_rifle_ammo01.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.ammo = "ee3"
+ITEM.ammoAmount = 50
+ITEM.description = "A Box that contains %s EE-3 cartridges"
+ITEM.category = "Ammunition"
