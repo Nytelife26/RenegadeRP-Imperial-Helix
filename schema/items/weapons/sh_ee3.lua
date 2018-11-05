5722ff8540eb1a146484301e46d73d4b59d3ce6e
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-05 21:51:59 +0000

    Create sh_ee3.lua

---
+++ schema/items/weapons/sh_ee3.lua
@@ -0,0 +1,11 @@
+
+ITEM.name = "EE-3 Carbine Rifle"
+ITEM.description = "A carbine rifle capable of long-range warfare."
+ITEM.category = "Weaponry"
+ITEM.model = "models/weapons/w_ee3.mdl"
+ITEM.class = "weapon_ee3"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.isWeapon = true
+ITEM.isGrenade = false
+ITEM.weaponCategory = "primary"
