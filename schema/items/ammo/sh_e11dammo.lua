7a1afc4322ba03e526c2ef8712e755ecb438436b
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-05 21:58:26 +0000

    Update sh_e11dammo.lua

--- schema/items/ammo/sh_e11dammo.lua
+++ schema/items/ammo/sh_e11dammo.lua
@@ -2,4 +2,5 @@ ITEM.name = "E-11D Pulses"
 ITEM.model = "models/items/combine_rifle_cartridge01.mdl"
 ITEM.ammo = "e11d" 
 ITEM.ammoAmount = 65 
-ITEM.ammoDesc = "A Cartridge that contains %s of E-11D Pulses"
+ITEM.description = "A Cartridge that contains %s of E-11D Pulses"
+ITEM.category = "Ammunition"
