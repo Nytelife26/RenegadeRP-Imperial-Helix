147f05f235a8f10a00ef392e097a80cd077e117c
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-06 16:24:54 +0000

    Create sh_schema.lua

---
+++ schema/sh_schema.lua
@@ -0,0 +1,148 @@
+
+Schema.name = "ImperialRP"
+Schema.author = "RenegadeRP"
+Schema.description = "A schema based on Star Wars."
+
+-- Include netstream
+ix.util.Include("libs/thirdparty/sh_netstream2.lua")
+
+ix.util.Include("sh_configs.lua")
+ix.util.Include("sh_commands.lua")
+
+ix.util.Include("cl_schema.lua")
+ix.util.Include("cl_hooks.lua")
+ix.util.Include("sh_hooks.lua")
+ix.util.Include("sh_voices.lua")
+ix.util.Include("sv_schema.lua")
+ix.util.Include("sv_hooks.lua")
+
+ix.util.Include("meta/sh_player.lua")
+ix.util.Include("meta/sv_player.lua")
+ix.util.Include("meta/sh_character.lua")
+
+ix.flag.Add("v", "Access to light blackmarket goods.")
+ix.flag.Add("V", "Access to heavy blackmarket goods.")
+
+function Schema:ZeroNumber(number, length)
+	local amount = math.max(0, length - string.len(number))
+	return string.rep("0", amount)..tostring(number)
+end
+
+function Schema:IsEmpireRank(text, rank)
+	return string.find(text, "[%D+]"..rank.."[%D+]")
+end
+
+
+do
+	local CLASS = {}
+	CLASS.color = Color(75, 150, 50)
+	CLASS.format = "%s radios in \"%s\""
+
+	function CLASS:CanHear(speaker, listener)
+		local character = listener:GetCharacter()
+		local inventory = character:GetInventory()
+
+		for k, v in pairs(inventory:GetItemsByUniqueID("portable_radio", true)) do
+			if (v:GetData("enabled", false)) then
+				return speaker:GetCharacter():GetData("frequency") == character:GetData("frequency")
+			end
+		end
+	end
+
+	function CLASS:OnChatAdd(speaker, text)
+		text = speaker:IsEmpire() and string.format("<:: %s ::>", text) or text
+		chat.AddText(self.color, string.format(self.format, speaker:Name(), text))
+	end
+
+	ix.chat.Register("radio", CLASS)
+end
+
+do
+	local CLASS = {}
+	CLASS.color = Color(255, 255, 175)
+	CLASS.format = "%s radios in \"%s\""
+
+	function CLASS:GetColor(speaker, text)
+		if (LocalPlayer():GetEyeTrace().Entity == speaker) then
+			return Color(175, 255, 175)
+		end
+
+		return self.color
+	end
+
+	function CLASS:CanHear(speaker, listener)
+		if (ix.chat.classes.radio:CanHear(speaker, listener)) then
+			return false
+		end
+
+		local chatRange = ix.config.Get("chatRange", 280)
+
+		return (speaker:GetPos() - listener:GetPos()):LengthSqr() <= (chatRange * chatRange)
+	end
+
+	function CLASS:OnChatAdd(speaker, text)
+		text = speaker:IsEmpire() and string.format("<:: %s ::>", text) or text
+		chat.AddText(self.color, string.format(self.format, speaker:Name(), text))
+	end
+
+	ix.chat.Register("radio_eavesdrop", CLASS)
+end
+
+do
+	local CLASS = {}
+	CLASS.color = Color(175, 125, 100)
+	CLASS.format = "%s requests \"%s\""
+
+	function CLASS:CanHear(speaker, listener)
+		return listener:IsEmpire() or speaker:Team() == FACTION_NAVY
+	end
+
+	function CLASS:OnChatAdd(speaker, text)
+		chat.AddText(self.color, string.format(self.format, speaker:Name(), text))
+	end
+
+	ix.chat.Register("request", CLASS)
+end
+
+do
+	local CLASS = {}
+	CLASS.color = Color(175, 125, 100)
+	CLASS.format = "%s requests \"%s\""
+
+	function CLASS:CanHear(speaker, listener)
+		if (ix.chat.classes.request:CanHear(speaker, listener)) then
+			return false
+		end
+
+		local chatRange = ix.config.Get("chatRange", 280)
+
+		return (speaker:Team() == FACTION_STORM and listener:Team() == FACTION_STORM)
+		and (speaker:GetPos() - listener:GetPos()):LengthSqr() <= (chatRange * chatRange)
+	end
+
+	function CLASS:OnChatAdd(speaker, text)
+		chat.AddText(self.color, string.format(self.format, speaker:Name(), text))
+	end
+
+	ix.chat.Register("request_eavesdrop", CLASS)
+end
+
+do
+	local CLASS = {}
+	CLASS.color = Color(150, 125, 175)
+	CLASS.format = "%s broadcasts \"%s\""
+
+	function CLASS:CanSay(speaker, text)
+		if (speaker:Team() != FACTION_NAVY) then
+			speaker:NotifyLocalized("notAllowed")
+
+			return false
+		end
+	end
+
+	function CLASS:OnChatAdd(speaker, text)
+		chat.AddText(self.color, string.format(self.format, speaker:Name(), text))
+	end
+
+	ix.chat.Register("broadcast", CLASS)
+end
