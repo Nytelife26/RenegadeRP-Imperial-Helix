9e5a49eb7a57426c9f17c064a582a71cadbe2f94
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-07 22:38:24 +0000

    Create sh_staff.lua

---
+++ schema/factions/sh_staff.lua
@@ -0,0 +1,6 @@
+FACTION.name = "Server Administration"
+FACTION.description = "A group of administrators and helpers happy to help with any queries or issues!"
+FACTION.color = Color(0, 255, 255)
+FACTION.pay = 0
+FACTION.isDefault = false
+FACTION.isGloballyRecognized = true
