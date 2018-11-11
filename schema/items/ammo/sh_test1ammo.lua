ff5401a860de49d4923c83b3d570d92b44cf02a8
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-11 17:24:05 +0000

    Create sh_test1ammo.lua

---
+++ schema/items/ammo/sh_test1ammo.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Shotgun Shells"
+ITEM.model = "models/Items/BoxBuckshot.mdl"
+ITEM.ammo = "buckshot"
+ITEM.ammoAmount = 15
+ITEM.ammoDesc = "A Box of %s Shotgun Shells"
+
+-- credits to AlexGrist for this. Simply a temporary use whilst we fix some stuff. --
