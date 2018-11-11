d2870e3033631a9f75850768e06191e2cd08fb67
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-11 17:22:55 +0000

    Create sh_test1.lua

---
+++ schema/items/weapons/sh_test1.lua
@@ -0,0 +1,14 @@
+ITEM.name = "Shotgun"
+ITEM.description = "A powerful pump-action shotgun."
+ITEM.model = "models/weapons/w_shotgun.mdl"
+ITEM.class = "weapon_shotgun"
+ITEM.weaponCategory = "primary"
+ITEM.width = 3
+ITEM.height = 1
+ITEM.iconCam = {
+    pos = Vector(0, 200, 1),
+    ang = Angle(0, 270, 0),
+    fov = 10
+}
+
+-- Credits to AlexGrist for this. Just a test to see issues within weapon making --
