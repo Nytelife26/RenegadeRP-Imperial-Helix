a2e73fa6eec297aa4017fe520372740b152b9674
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-14 16:34:26 +0000

    Create sh_pulses.lua

---
+++ schema/items/ammo/sh_pulses.lua
@@ -0,0 +1,5 @@
+ITEM.name = "Imperial Pulses"
+ITEM.model = "models/Items/combine_rifle_cartridge01.mdl"
+ITEM.ammo = "tfa_ammo_ar2"
+ITEM.ammoAmount = 50
+ITEM.ammoDesc = "A Box that contains %s of Imperial Pulses"
