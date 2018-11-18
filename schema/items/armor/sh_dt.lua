4fb708476b5dd132bada5d373c2f626ab5b7d949
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-18 12:48:29 +0000

    Delete sh_dt.lua

--- schema/items/armor/sh_dt.lua
+++
@@ -1,9 +0,0 @@
-
-ITEM.name = "Death Trooper Uniform"
-ITEM.description = "A classified and vastly unknown black plastoid composite coated with various battlefield contraptions."
-ITEM.category = "Armor"
-ITEM.model = "models/player/hydro/swbf_deathtrooper/swbf_deathtrooper3.mdl"
-ITEM.width = 1
-ITEM.height = 1
-ITEM.outfitCategory = "Empire"
-ITEM.pacData = {}
