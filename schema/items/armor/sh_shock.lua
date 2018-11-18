a00461517158217d405dab068fc05b7005e768d6
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-18 12:48:43 +0000

    Delete sh_shock.lua

--- schema/items/armor/sh_shock.lua
+++
@@ -1,9 +0,0 @@
-
-ITEM.name = "Shock Battalion Uniform"
-ITEM.description = "A red and white plastoid composite worn over a black body glove."
-ITEM.category = "Armor"
-ITEM.model = "models/player/hydro/incinerator_stormtrooper/incinerator_stormtrooper.mdl"
-ITEM.width = 1
-ITEM.height = 1
-ITEM.outfitCategory = "Empire"
-ITEM.pacData = {}
