36d0729a0dee1803b61342d5fb7adcc62a58576a
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-18 12:45:44 +0000

    Delete sh_501st.lua

--- schema/items/armor/sh_501st.lua
+++
@@ -1,9 +0,0 @@
-
-ITEM.name = "501st Legion Uniform"
-ITEM.description = "A A white and blue plastoid composite worn over a black body glove, adorned with the mark of Vader's Fist."
-ITEM.category = "Armor"
-ITEM.model = "models/player/venator/501st_stormtrooper/501st_stormtrooper.mdl"
-ITEM.width = 1
-ITEM.height = 1
-ITEM.outfitCategory = "Empire"
-ITEM.replacements = {"group01"}
