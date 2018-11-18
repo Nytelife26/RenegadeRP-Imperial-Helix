9599ef0b0dc56a96ff1823d61aae8df3ffd3109b
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-18 12:48:49 +0000

    Delete sh_stormtrooper.lua

--- schema/items/armor/sh_stormtrooper.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Storm Trooper Uniform"
-ITEM.description = "A white plastoid composite worn over a black body glove."
-ITEM.category = "Armor"
-ITEM.model = "models/player/venator/anh2/anh_stormtrooper2.mdl"
-ITEM.width = 1
-ITEM.height = 1
-ITEM.outfitCategory = "Empire"
