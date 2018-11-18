4ad8260e70eb1a7e045d3ab49659e3ff2fd22cd2
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-18 12:48:35 +0000

    Delete sh_scout.lua

--- schema/items/armor/sh_scout.lua
+++
@@ -1,9 +0,0 @@
-
-ITEM.name = "Scout Battalion Uniform"
-ITEM.description = "A specialized uniform, designed for stealth and scouting operations."
-ITEM.category = "Armor"
-ITEM.model = "models/sono/swbf3/backpack.mdl"
-ITEM.width = 1
-ITEM.height = 1
-ITEM.outfitCategory = "Empire"
-ITEM.pacData = {}
