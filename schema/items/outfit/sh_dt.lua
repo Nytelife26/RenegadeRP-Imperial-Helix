a88e76b90fa69c093eaf7401f0739d605be14bfe
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-20 16:51:20 +0000

    Update sh_dt.lua

--- schema/items/outfit/sh_dt.lua
+++ schema/items/outfit/sh_dt.lua
@@ -9,3 +9,14 @@ ITEM.outfitCategory = "Empire"
 ITEM.pacData = {}
 ITEM.newSkin = 1
 ITEM.replacements = "models/npc/swbf_deathtrooper/swbf_deathtrooper2_npc.mdl"
+
+ITEM.functions.equip = {
+	OnRun = function(itemTable)
+		local client = itemTable.player
+
+		client:RestoreStamina(35)
+        client:SetHealth(math.Clamp(client:Health() + 100, 100, client:GetMaxHealth()))
+        client:SetArmor(math.clamp(client:Armor() + 100, 100, client:GetMaxArmor()))
+		client:EmitSound("buttons/button19.wav", 75, 90, 0.35)
+	end
+}
