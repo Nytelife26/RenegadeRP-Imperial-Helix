f7a33642f9a023fba889b39979c6e30c402e1c82
Nytelife26 <nytelife@protonmail.com>
2018-11-20 18:21:47 +0000

    Removed test ammo and weapon

--- schema/items/ammo/sh_test1ammo.lua
+++
@@ -1,7 +0,0 @@
-ITEM.name = "Shotgun Shells"
-ITEM.model = "models/Items/BoxBuckshot.mdl"
-ITEM.ammo = "buckshot"
-ITEM.ammoAmount = 15
-ITEM.ammoDesc = "A Box of %s Shotgun Shells"
-
--- credits to AlexGrist for this. Simply a temporary use whilst we fix some stuff. --
