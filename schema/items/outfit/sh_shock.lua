6ea717529d125afadd81d989e8db50b27d26312c
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-22 17:12:08 +0000

    Update sh_shock.lua

--- schema/items/outfit/sh_shock.lua
+++ schema/items/outfit/sh_shock.lua
@@ -2,8 +2,10 @@
 ITEM.name = "Shock Battalion Uniform"
 ITEM.description = "A red and white plastoid composite worn over a black body glove."
 ITEM.category = "Armor"
-ITEM.model = "models/player/hydro/incinerator_stormtrooper/incinerator_stormtrooper.mdl"
+ITEM.model = "models/props_junk/cardboard_box001a.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.outfitCategory = "Empire"
 ITEM.pacData = {}
+ITEM.newSkin = 1
+ITEM.replacements = "models/player/hydro/incinerator_stormtrooper/incinerator_stormtrooper.mdl"
