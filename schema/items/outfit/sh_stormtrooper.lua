6a2fc61a045c069c85ecec49580ed5a0380aa3ab
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-22 17:09:56 +0000

    Update sh_stormtrooper.lua

--- schema/items/outfit/sh_stormtrooper.lua
+++ schema/items/outfit/sh_stormtrooper.lua
@@ -1,7 +1,9 @@
 ITEM.name = "Storm Trooper Uniform"
 ITEM.description = "A white plastoid composite worn over a black body glove."
 ITEM.category = "Armor"
-ITEM.model = "models/player/venator/anh2/anh_stormtrooper2.mdl"
+ITEM.model = "models/props_junk/cardboard_box001a.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.outfitCategory = "Empire"
+ITEM.newSkin = 1
+ITEM.replacements = "models/player/venator/anh2/anh_stormtrooper2.mdl"
