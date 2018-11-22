1c23de355c5eac3aeeb4d0785cd55191bf6f4637
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-22 17:08:40 +0000

    Update sh_501st.lua

--- schema/items/outfit/sh_501st.lua
+++ schema/items/outfit/sh_501st.lua
@@ -2,8 +2,10 @@
 ITEM.name = "501st Legion Uniform"
 ITEM.description = "A white and blue plastoid composite worn over a black body glove, adorned with the mark of Vader's Fist."
 ITEM.category = "Armor"
-ITEM.model = "models/player/venator/501st_stormtrooper/501st_stormtrooper.mdl"
+ITEM.model = "models/props_junk/cardboard_box003a.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.outfitCategory = "Empire"
 ITEM.replacements = {"group01"}
+ITEM.newSkin = 1
+ITEM.replacements = "models/player/venator/501st_stormtrooper/501st_stormtrooper.mdl"
