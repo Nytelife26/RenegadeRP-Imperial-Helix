4137f28c9daf976519e320cd36c3db7ba008bc33
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-23 17:11:21 +0000

    Update sh_scout.lua

--- schema/items/outfit/sh_scout.lua
+++ schema/items/outfit/sh_scout.lua
@@ -2,10 +2,10 @@
 ITEM.name = "Scout Battalion Uniform"
 ITEM.description = "A specialized uniform, designed for stealth and scouting operations."
 ITEM.category = "Armor"
-ITEM.model = "models/sono/swbf3/backpack.mdl"
+ITEM.model = "models/props_junk/cardboard_box001b.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.outfitCategory = "Empire"
 ITEM.pacData = {}
 ITEM.newSkin = 1
-ITEM.replacements = "models/props_junk/cardboard_box001b.mdl"
+ITEM.replacements = "models/sono/swbf3/backpack.mdl"
