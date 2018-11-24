6728786b98284d44dc4b7b9f82f9878b0bb3f446
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-24 13:37:15 +0000

    Create sh_dl44.lua

---
+++ schema/items/weapons/sh_dl44.lua
@@ -0,0 +1,13 @@
+ITEM.name = "DL-44 Heavy Blaster Pistol"
+ITEM.description = "One of the most powerful blaster pistols in the Galaxy, this weapon is courtesy of BlasTech Industries."
+ITEM.category = "Weaponry"
+ITEM.model = "models/weapons/w_dl44.mdl"
+ITEM.class = "tfa_dl44"
+ITEM.weaponCategory = "secondary"
+ITEM.width = 2
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}
