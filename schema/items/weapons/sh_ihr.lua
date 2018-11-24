7bde0c15f42d062cd7048dc69bc82cd099daed3a
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-24 13:41:38 +0000

    Create sh_ihr.lua

---
+++ schema/items/weapons/sh_ihr.lua
@@ -0,0 +1,13 @@
+ITEM.name = "Imperial Heavy Repeater"
+ITEM.description = "A projectile weapon utilised by the highest ranking of the enlisted."
+ITEM.category = "Weaponry"
+ITEM.model = "models/weapons/w_ihr.mdl"
+ITEM.class = "tfa_752_ihr"
+ITEM.weaponCategory = "primary"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}
