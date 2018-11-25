40f6df2f6d0a7e01ebf920907125c8827d9c740d
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-25 13:31:43 +0000

    Create sh_dlt20a.lua

---
+++ schema/items/weapons/sh_dlt20a.lua
@@ -0,0 +1,13 @@
+ITEM.name = "DLT-20A Blaster Rifle"
+ITEM.description = "A highly powerful long-range blaster rifle manufactured by BlasTech industries."
+ITEM.category = "Weaponry"
+ITEM.model = "models/weapons/synbf3/w_dlt20a.mdl"
+ITEM.class = "tfa_dlt20a_extended"
+ITEM.weaponCategory = "primary"
+ITEM.width = 4
+ITEM.height = 2
+ITEM.iconCam = {
+	ang	= Angle(-0.70499622821808, 268.25439453125, 0),
+	fov	= 12.085652091515,
+	pos	= Vector(0, 200, 0)
+}
