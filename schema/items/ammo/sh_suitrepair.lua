45880849684aae498eb9ced150c74b54790abc6e
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-30 16:57:31 +0000

    Create sh_suitrepair.lua

---
+++ schema/items/ammo/sh_suitrepair.lua
@@ -0,0 +1,5 @@
+ITEM.name = "Uniform Repair Kit"
+ITEM.model = "models/Items/car_battery01.mdl"
+ITEM.ammo = "item_battery"
+ITEM.ammoAmount = 1
+ITEM.ammoDesc = "A standard Imperial uniform repair kit."
