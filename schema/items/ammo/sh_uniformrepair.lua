1b5d6c4f964ea7cdeb524216cd0317d769973f42
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-30 17:07:32 +0000

    Delete sh_uniformrepair.lua

--- schema/items/ammo/sh_uniformrepair.lua
+++
@@ -1,5 +0,0 @@
-ITEM.name = "Uniform Repair Kit"
-ITEM.model = "models/Items/car_battery01.mdl"
-ITEM.ammo = "item_battery"
-ITEM.ammoAmount = 1
-ITEM.ammoDesc = "A standard Imperial uniform repair kit."
