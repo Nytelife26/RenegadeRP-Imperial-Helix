20a5f7f9e7b043b88d8176b43795aeb7c53a3204
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-11-30 17:07:09 +0000

    Create sh_uniformrepair.lua

---
+++ schema/items/sh_uniformrepair.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Standard Imperial Uniform Repair Kit"
+ITEM.description = "A single Standard Imperial Uniform Repair Kit."
+ITEM.category = "Medical"
+ITEM.model = "models/Items/car_battery01.mdl"
+ITEM.class = "item_battery"
+ITEM.width = 1
+ITEM.height = 1
