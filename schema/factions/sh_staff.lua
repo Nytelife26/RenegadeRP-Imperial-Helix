e95c1f64f117c3c453377eaa6edac9618cb51176
JKFinnigan <44735095+JKFinnigan@users.noreply.github.com>
2018-12-08 22:10:30 +0000

    Update sh_staff.lua

--- schema/factions/sh_staff.lua
+++ schema/factions/sh_staff.lua
@@ -1,6 +1,6 @@
 FACTION.name = "Server Administration"
 FACTION.description = "A group of administrators and helpers happy to help with any queries or issues!"
-FACTION.color = Color(0, 255, 255)
+FACTION.color = Color(230, 94, 96)
 FACTION.pay = 0
 FACTION.isDefault = false
 FACTION.isGloballyRecognized = true
